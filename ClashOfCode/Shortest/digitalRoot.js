34a39815cfbf398afb69eb8b7da51aff016bf54e
Pandafuchs <github@notzon.me>
2015-12-11 15:33:51 +0100

    alternative c++

--- ClashOfCode/Shortest/digitalRoot.js
+++ ClashOfCode/Shortest/digitalRoot.js
@@ -1,1 +1,14 @@
 function r(a){var b=0;return 9>=a?a:(a.toString().split("").forEach(function(a){return b+=parseInt(a)}),r(b))}print(r(parseInt(readline())));
+
+
+//Vielleicht auch:
+
+#include <stdio.h>
+
+int main(void)
+{
+   int number;
+   scanf("%d", &number);
+
+   printf("The digital root of %d is %d.", number, (1 + (number - 1) % 9));
+}
