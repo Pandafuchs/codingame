a15346953d1b9bcb4aa3608fcf662cf0f5ae920a
Pandafuchs <github@notzon.me>
2015-12-14 11:18:43 +0100

    minimize

--- ClashOfCode/Fastest/durationToInt.php
+++ ClashOfCode/Fastest/durationToInt.php
@@ -1,5 +1,4 @@
-<?php
-fscanf(STDIN, "%s", $duration);
-$a = explode(':', $duration);
-$i = $a[0] * 60 + $a[1];
-echo $i;
+<?
+fscanf(STDIN, "%s", $d);
+$a = explode(':', $d);
+echo $a[0] * 60 + $a[1];
